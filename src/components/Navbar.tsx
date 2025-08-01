cbd84ca2230a014e1777e74d8245e03265d1f7ce
Prompan Uechanwech <ueprompan@central.co.th>
2025-08-01 11:30:23 +0700

    update button and adding icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import Button from "./Button";
 import Header from "./Header";
+import { FaCirclePlus } from "react-icons/fa6";
 
 export default function Navbar() {
     return(
         <>
-            <nav className="flex justify-between items=center px-6 py-4 text-white shadow-md">
+            <nav className="flex justify-between items=center p-4 text-white shadow-md">
                 <div>
                     <Header />
                 </div>
                 <div>
-                    <Button label="Create" type="button" />
+                    <Button icon={<FaCirclePlus />} label="Create" type="button" />
                 </div>
             </nav>
         </>
