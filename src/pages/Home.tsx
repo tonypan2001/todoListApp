fdeddff26b64655f36f27ca6be0110854561901d
Prompan Uechanwech <panzaza9543@gmail.com>
2025-08-06 21:07:18 +0700

    add complete sign

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,6 +15,16 @@ export default function Home() {
                         <Button icon={<FaSearch />} label="Search" />
                     </div>
                 </div>
+                <div className="flex items-start">
+                    <div className="w-[60px] h-[60px] flex items-center justify-center border-2 rounded-4xl shadow-lg">
+                        <h1 className="font-bold">100%</h1>
+                    </div>
+                    <div className="flex flex-col items-start ms-3">
+                        <p className="font-bold text-xl">Your have 1 task(s) to complete</p>
+                        <p className="text-sm">No tasks completed yet. Keep going!</p>
+                    </div>
+                </div>
+                <div></div>
             </div>
         </>
     )
