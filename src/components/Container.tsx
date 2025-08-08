41912650fd52d3a71b3a434b16ae6ff6acb207d3
Prompan Uechanwech <ueprompan@central.co.th>
2025-08-08 10:14:22 +0700

    edit class

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import type { ContainerProps } from "../types/container.types";
 
 export default function Container(props: ContainerProps) {
     return(
-        <div className={`flex justify-between items-center p-4 shadow-md rounded-xl bg-[var(--primary-dark)] border border-[var(--primary-dark-hover)] ${props.className}`}>
+        <div className={`flex justify-between items-center p-4 shadow-md rounded-xl bg-white dark:bg-[var(--primary-dark)] border border-gray-200 dark:border-[var(--primary-dark-hover)] ${props.className}`}>
             {props.children}
         </div>
     )
