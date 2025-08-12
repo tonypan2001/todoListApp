d46adb38394a03a3ab4baeddefc1da63cc03749d
Prompan Uechanwech <ueprompan@central.co.th>
2025-08-12 12:34:37 +0700

    create spinner compo

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,5 @@
+export default function Spinner() {
+  return (
+    <div className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-gray-300 border-t-[var(--primary-color)] align-middle" />
+  );
+}
